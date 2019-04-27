51dd95ec9665e925fc9ae24c9c9c6d3d0fef22d6
Joan Awinja Ingari <joan.awinja@andela.com>
2019-04-27 23:27:35 +0300

    db url

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!flask/bin/python
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/airtech-api'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DATABASE_URL") 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 CORS(app)
